dac3025e189da3ec7cc6f4914e9fa030a633037f
philipomoigui <philipomoigui@gmail.com>
2021-02-04 23:00:23 +0000

    Completed advanced tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description - This function prints out some info
+ * to the standard error
+ */
+
+int main()
+{
+  fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\nc");
+  return 1;
+}
