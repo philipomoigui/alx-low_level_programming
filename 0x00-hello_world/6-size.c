cbec019a6742a0daae4f0737331c488eb5e16757
philipomoigui <philipomoigui@gmail.com>
2021-02-05 00:08:04 +0000

    Corrected size of c program

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,7 @@
  * Description: This function prints out the sizes of various
  * data types in C language.
  *
- *Return: 0
+ *Return: Always 0 (Success)
  */
 
 int main(void)
@@ -17,5 +17,6 @@ printf("Size of a int: %lu byte(s)\n", sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 printf("Size of a float: %lu byte(s)\n", sizeof(float));
+
 return (0);
 }
