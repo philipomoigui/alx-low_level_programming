961c8fcea1d2f81c8a489fc984704224990769f0
philipomoigui <philipomoigui@gmail.com>
2021-02-12 20:18:01 +0000

    Corrected the 5th solution

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,14 +11,15 @@ void more_numbers(void)
 {
 int i = 0, j;
 
- while (i < 10)
-   {
-     for (j = 0; j < 14; j++)
-       {
-	 if (j > 9)
-	   _putchar('0' + j/10);
-
-	 _putchar('0' + j % 10);
-       }
-     _putchar('\n');
-   }
+while (i < 10)
+{
+for (j = 0; j < 15; j++)
+{
+if (j > 9)
+_putchar('0' + j / 10);
+_putchar('0' + j % 10);
+}
+i++;
+putchar('\n');
+}
+}
