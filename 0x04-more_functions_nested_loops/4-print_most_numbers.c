3909e1a5250d1637d88cf3c3a5a533aa145312fb
philipomoigui <philipomoigui@gmail.com>
2021-02-12 20:57:57 +0000

    Corrected print more numbers solution

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,15 +6,16 @@
  * Return: no return value
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 int i;
+
 for (i = 48; i < 58; i++)
 {
 if (i == 50 || i == 52)
 continue;
 
-putchar(i);
+_putchar(i);
 }
 _putchar('\n');
 }
