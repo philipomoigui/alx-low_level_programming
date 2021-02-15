11d6656897d016189c2454fe970ddbfee6b4466d
philipomoigui <philipomoigui@gmail.com>
2021-02-15 21:54:24 +0000

    Added first exercise solution

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - Takes a pointer to an int as parameter
+ * and update the value it points to, to 98
+ *
+ * @n: a pointer to the integer we want to set to 98
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,7 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+void reste_to_98(int *n);
+
+
+#endif /* HOLBERTON_H */
