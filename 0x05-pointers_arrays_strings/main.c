d297ea463538a977270deb71b3d3257aaac8461b
philipomoigui <philipomoigui@gmail.com>
2021-02-16 11:22:03 +0000

    More corrections

--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -3,10 +3,13 @@
 /**
  * main - to test the solutions
  *
- * Return: 0 always
+ * Return: 0 on success
  */
 
 int main(void)
 {
-
+  char *str;
+  str = "Holberton is a school";
+ _puts(str);
+ return (0);
 }
