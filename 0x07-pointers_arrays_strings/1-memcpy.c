5d686f13bb7c0654c9a3e259f70fdf1da275e735
philipomoigui <philipomoigui@gmail.com>
2021-02-22 20:03:24 +0000

    Added second solution file

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _memcpy - function that copies some bytes from memory area to another
+ * @src: character to copy from
+ * @dest: character to copy to
+ * @n: the first bytes in memory to be filled.
+ * Return: s
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; dest[i] != '\0'; i++)
+{
+if (i < n)
+dest[i] = src[i];
+}
+
+return (dest)
+}
