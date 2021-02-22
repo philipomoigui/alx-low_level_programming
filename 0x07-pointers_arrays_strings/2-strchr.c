06ea54336c94fd7bec8464059034aa6f07a790e8
philipomoigui <philipomoigui@gmail.com>
2021-02-22 20:24:58 +0000

    Added third solution

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _strchr - function to return a pointer to the first occurence
+ * of a character if found
+ *
+ * @s: string to be checked
+ * @c: character to check if exist
+ *
+ * Return: a pointer to the char on success else 0
+ */
+
+char *_strchr(char *s, char c)
+{
+
+unsigned int i;
+for (i = 0; s[i] != '/0'; i++)
+{
+if (s[i] == c)
+return (s[i]);
+
+}
+return (0);
+}
