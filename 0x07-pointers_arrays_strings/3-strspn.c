67b7d28a84fb56e0529ac94a917b71d709d84da7
philipomoigui <philipomoigui@gmail.com>
2021-02-22 20:45:27 +0000

    Changed third solution name commit name

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string to be checked
+ * @accept: string to check
+ * Return: number of bytes
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+  int i, j, k, flag;
+
+  k = 0;
+
+  for (i = 0; s[i] != accept[j]; i++)
+    {
+      flag = 0;
+
+      for (j = 0; accept[j] != '\0'; j++)
+
+	if (s[i] == accept[j])
+	  {
+	    k++;
+
+	    flag = 1;
+	  }
+      if (flag == 0)
+	{
+	  return (k);
+	}
+    }
+  return (0);
+}
