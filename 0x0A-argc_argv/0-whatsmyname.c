10abbfe7d388e50656c8aa43b4efd7f19eaf078e
philipomoigui <philipomoigui@gmail.com>
2021-02-28 11:24:53 +0000

    Program to print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - a program that prints its name
+ * @argc: argument count
+ * @argv: array of arguments
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+  printf("%s\n", argv[0]);
+}
