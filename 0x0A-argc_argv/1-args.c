a23c474b7aa8f9b2f6986635b16a5f3991f45215
philipomoigui <philipomoigui@gmail.com>
2021-02-28 14:53:49 +0000

    Added more solution files

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - Prints the number of argument passed to it
+ * @argc: counts of argument
+ * @argv: array of arguments
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv)
+{
+(void)argv;
+printf("%d\n", argc - 1);
+return (0);
+}
