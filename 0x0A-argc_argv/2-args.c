836c00dafec60a5ceefd3847c4ba8fcc078cf996
philipomoigui <philipomoigui@gmail.com>
2021-02-28 15:20:20 +0000

    Added more solution files

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -16,4 +16,5 @@ for (i = 0; i < argc; i++)
 {
 printf("%s\n", argv[i]);
 }
+return (0);
 }
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a function that multiplies two arguemnts
+ * @argc: count of arguments
+ * @argv: array of arguments
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+int total = 1, i;
+
+if (argc == 3)
+{
+for (i = 1; i < argc; i++)
+total *= atoi(argv[i]);
+
+printf("%d\n",total);
+return (0);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+}
