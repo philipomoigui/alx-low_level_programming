fd2ef92cec72fcefcb0f587def85af0121f73e0d
philipomoigui <philipomoigui@gmail.com>
2021-03-02 20:59:10 +0000

    Corrected files

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,9 +14,9 @@ int **grid, i, j;
 
 if (width <= 0 || height <= 0)
 return (NULL);
-grid = (int **)malloc(height * sizeof(int*));
+grid = (int **)malloc(height * sizeof(int *));
 for (i = 0; i < height; i++)
-grid[i] = (int*)malloc(width * sizeof(int));
+grid[i] = (int *)malloc(width * sizeof(int));
 if (grid == NULL)
 return (NULL);
 for (i = 0; i < height; i++)
